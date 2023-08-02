7ecfbc906ef0fbc8c81b7927508af57b78896fbf
alsensto <jmg1301@gmail.com>
2023-08-02 19:45:03 +0900

    a

--- src/main/java/com/example/hotelk/security/SecurityConfig.java
+++ src/main/java/com/example/hotelk/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/hotelFacility/**").permitAll()
                         .requestMatchers("/api/v1/hotelFacilityUsage/**").permitAll()
                         .requestMatchers("/api/v1/amenities/**").permitAll()
+                        .requestMatchers("/api/v1/**").permitAll()
                         .anyRequest().authenticated()
                 );
 //        http
